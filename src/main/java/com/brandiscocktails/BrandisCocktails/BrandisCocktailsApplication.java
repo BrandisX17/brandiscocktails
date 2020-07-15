9ec564128da27e10af954416dd8f89b47c71ada7
Brandis <branokolar@gmail.com>
2020-07-15 13:57:42 +0200

    Spring Initializr

---
+++ src/main/java/com/brandiscocktails/BrandisCocktails/BrandisCocktailsApplication.java
@@ -0,0 +1,13 @@
+package com.brandiscocktails.BrandisCocktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandisCocktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandisCocktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandiscocktails/BrandisCocktails/BrandisCocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandiscocktails.BrandisCocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandisCocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
